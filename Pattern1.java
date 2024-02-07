ee0c0d8ea01f2669d57889f1dd8ee010f7910d1b
tazul9770 <linkedin52425@gmail.com>
2024-02-07 10:48:37 +0600

    Pattern1

---
+++ Pattern1.java
@@ -0,0 +1,13 @@
+//print solid rectangle
+public class Pattern1 {
+    public static void main(String[] args) {
+        //outer loop
+        for(int i = 1; i <= 5; i++) {
+            // inner loop 
+            for(int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
