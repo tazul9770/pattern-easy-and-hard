bd2d901f9c754d4543ca4780d45ce8d1e445e7ca
tazul9770 <linkedin52425@gmail.com>
2024-02-07 10:56:45 +0600

    change

--- Pattern1.java
+++ Pattern1.java
@@ -2,7 +2,7 @@
 public class Pattern1 {
     public static void main(String[] args) {
         //outer loop
-        for(int i = 1; i <= 5; i++) {
+        for(int i = 1; i <= 4; i++) {
             // inner loop 
             for(int j = 1; j <= 5; j++) {
                 System.out.print("*");
