df5ae96be72f6ba26beb4c4e49b52594bc39df41
tazul9770 <linkedin52425@gmail.com>
2024-02-11 19:23:06 +0600

    Pattern11

---
+++ Pattern11.java
@@ -0,0 +1,14 @@
+// solid Rhombus
+public class Pattern11{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 5; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
